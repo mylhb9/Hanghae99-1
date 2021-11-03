77a30905e15417b783516ec30c26fdbdf7a27e12
mylhb9 <mylhb9@naver.com>
2021-11-03 18:13:28 +0900

    no message

--- static/myjs.js
+++ static/myjs.js
@@ -137,8 +137,58 @@ function get_posts(username) {
         }
     })
 }
-$('#myModal').on('shown.bs.modal', function () {
-  $('#myInput').trigger('focus')
-})
 
+function get_stars(username) {
+    if (username == undefined) {
+        username = ""
+    }
+    $("#post-box").empty()
+    $.ajax({
+        type: "GET",
+        // url: "/get_posts",
+        url: `/get_posts?username_give=${username}`,
+        data: {},
+        success: function (response) {
+            if (response["result"] == "success") {
+                let posts = response["posts"]
+                for (let i = 0; i < posts.length; i++) {
+                    let post = posts[i]
+                    let time_post = new Date(post["date"])
+                    let time_before = time2str(time_post)
+                    let class_heart = post['heart_by_me'] ? "fa-heart" : "fa-heart-o"
+                    let count_heart = post['count_heart']
+                    let html_temp = `<div class="box" id="${post["_id"]}">
+                                        <article class="media">
+                                            <div class="media-left">
+                                                <a class="image is-64x64" href="/user/${post['username']}">
+                                                    <img class="is-rounded" src="/static/${post['profile_pic_real']}"
+                                                         alt="Image">
+                                                </a>
+                                            </div>
+                                            <div class="media-content">
+                                                <div class="content">
+                                                    <p>
+                                                        <strong>${post['profile_name']}</strong> <small>@${post['username']}</small> <small>${time_before}</small>
+                                                        <br>
+                                                        ${post['comment']}
+                                                    </p>
+                                                </div>
+                                                <nav class="level is-mobile">
+                                                    <div class="level-left">
+                                                        <a class="level-item is-sparta" aria-label="heart" onclick="toggle_like('${post['_id']}', 'heart')">
+                                                            <span class="icon is-small"><i class="fa  ${class_heart}"
+                                                                                           aria-hidden="true"></i></span>&nbsp;<span class="like-num">${num2str(count_heart)}</span>
+                                                        </a>
+                                                    </div>
+
+                                                </nav>
+                                            </div>
+                                        </article>
+                                    </div>`
+                    $("#post-box").append(html_temp)
+                }
+            }
+        }
+    })
+}
 
